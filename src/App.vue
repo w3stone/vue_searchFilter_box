6626d7061301fb678dd7a1412478bebae195767b
wjl <wjl.1993@gmail.com>
2018-08-06 15:06:27 +0800

    2018-08-06

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,95 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" type="text/css">
+    #app {
+        font-family: Arial,'Microsoft YaHei';
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    html,body{
+        font-size: 14px;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+        overflow-y: auto;
+    }
+    .clearfix::after{
+        content:"";
+        display: table;
+        clear: both;
+    }
+    .clearfix{
+        *zoom:1;
+    }
+    h1,h2,h3,h4,h5,h6{
+        margin: 0;
+        padding: 0;
+        cursor: default;
+    }
+    div,p{
+        margin: 0;
+        padding: 0;
+    }
+    img{
+        width: 100%;
+        vertical-align: middle;
+    }
+    i{
+        font-style: normal;
+    }
+    ul,ol,ul li,ol li{
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        cursor: default;
+    }
+    a{
+        text-decoration: none;
+        &:focus{
+            outline: none;
+        }
+    }
+    input,button{
+        border: none;
+        outline: none;
+    }
+    .center{
+        position: absolute;
+        margin: auto;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+    .updownCneter{
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+    /**设置滚动条的样式**/
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+    //滚动槽
+    ::-webkit-scrollbar-track {
+        background: #f3f3f3;
+        //border: 1px solid #bbb;
+        border-radius: 10px;
+    }
+    //滚动条滑块
+    ::-webkit-scrollbar-thumb {
+        border-radius: 10px;
+        background: #ccc;
+    }
 </style>
